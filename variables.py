05f33e2a0789c003ea1115dc19e1d1134cca057d
asma94khan <36408693+asma94khan@users.noreply.github.com>
2020-03-02 15:18:13 +0530

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,49 @@
+{
+  "Cloud": [
+    {
+      "AWS": "True",
+      "START": "True",
+      "STOP": "False",
+      "CLUSTER": "HDFS",
+      "LIST_OF_AWS_VMS": [
+        "i-01357669ed2977ds3",
+        "i-01357669ed2977cb4"
+      ],
+      "AMBARI_DOMAIN": "172.19.16.81",
+      "AMBARI_PORT": "8080",
+      "AMBARI_USER_ID": "admin",
+      "AMBARI_USER_PW": "admin",
+      "HDFS_CLUSTER_NAME": "test_cluster",
+      "CLUSTER_IPS": [
+        "172.19.16.81",
+        "172.19.16.82",
+        "172.19.16.83"
+      ]
+    },
+    {
+      "AZURE": "False",
+      "START": "False",
+      "STOP": "False",
+      "AZURE_SUBSCRIPTION_ID": "f9e4ead0-5e9f-47b9-b27d-2bfcabc3d2f1",
+      "AZURE_CLIENT_ID": "9a986622-3524-4d13-bac4-33dc8874e15b",
+      "AZURE_CLIENT_SECRET": "0XRO:E6-W5?CkuY8SzyuUu]z:mPZpjEd",
+      "AZURE_TENANT_ID": "d2b78f6f-af45-4f26-abc9-2fa9268975bc",
+      "LIST_OF_AZURE_VMS": [
+        "vmName1",
+        "vmName2"
+      ],
+      "GROUP_NAME": "test",
+      "CLUSTER": "KUBE",
+      "AMBARI_DOMAIN": "172.19.16.81",
+      "AMBARI_PORT": "8080",
+      "AMBARI_USER_ID": "admin",
+      "AMBARI_USER_PW": "admin",
+      "HDFS_CLUSTER_NAME": "test_cluster",
+      "CLUSTER_IPS": [
+        "172.19.16.81",
+        "172.19.16.82",
+        "172.19.16.83"
+      ]
+    }
+  ]
+}
